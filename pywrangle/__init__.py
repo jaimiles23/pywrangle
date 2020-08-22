3bfb28c363f945c61894ed9c33bcb145be6b75c0
Jai <jaimiles23@gmail.com>
2020-08-22 13:45:12 -0700

    spaced out import structure

--- pywrangle/__init__.py
+++ pywrangle/__init__.py
@@ -32,10 +32,16 @@ import numpy as np
 try:
     from pywrangle.string_cleaning import clean_str_columns
     from pywrangle.missing_data import show_col_nulls
-    from pywrangle.df_changes import record_df_info, print_df_changes
+    from pywrangle.df_changes import (
+        record_df_info,
+        print_df_changes
+    )
     
 except ModuleNotFoundError:
     from string_cleaning import clean_str_columns
     from missing_data import show_col_nulls
-    from df_changes import record_df_info, print_df_changes
+    from df_changes import (
+        record_df_info,
+        print_df_changes
+    )
 
