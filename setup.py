30f3e7288c2b50c6219899a044c870f812835bd6
Jai <jaimiles23@gmail.com>
2020-08-22 20:45:40 -0700

    -u version num

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ from setuptools import setup, find_packages
 ##########
 
 name = "pywrangle"
-version = "0.2.42"
+version = "0.2.46"
 description = "Auxiliary functions to clean pandas data frames"
 author = "Jai Miles"
 author_email = "jaimiles23@gmail.com"
