4b75d8fad27088144f1c2edbb2886fecac2d28e8
Jai <jaimiles23@gmail.com>
2020-10-30 18:14:48 -0700

    updated doc

--- pywrangle/df_changes/print_df_diff.py
+++ pywrangle/df_changes/print_df_diff.py
@@ -2,4 +2,11 @@
     - Columns
     - Rows
     - Size
+
+
+Note: Should be able to pass list iterable.
+For each one, print out size, etc.
+
+Then, if length of the list is only 2, THAT'S WHEN print 3rd row comparing.
 """
+
