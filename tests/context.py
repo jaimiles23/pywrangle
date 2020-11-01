3ae0e69c3841f62d1b4bb2f19e3ba4c00c927f4c
Jai <jaimiles23@gmail.com>
2020-11-01 11:11:55 -0800

    fixed create_df import & added notes on import reasons

--- tests/context.py
+++ tests/context.py
@@ -2,7 +2,12 @@ from __future__ import absolute_import
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pywrangle
-import create_df
+
+try:    # imports for individual test scripts
+    import create_df
+
+except ModuleNotFoundError:     # pytest import needs to be relative
+    from . import create_df
