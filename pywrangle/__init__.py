2482a29cbb967d8d5df95a0483a07250ce64a36a
Jai <jaimiles23@gmail.com>
2020-11-01 09:34:32 -0800

    added df change methods

--- pywrangle/__init__.py
+++ pywrangle/__init__.py
@@ -1,3 +1,5 @@
-from .str_cleaning.clean_strcol import clean_strcol
-from .str_cleaning.clean_all_strcol import clean_all_strcols
+from .df_changes.print_df_info import print_df_info
+from .df_changes.record_df_info import record_df_info
 from .print_tbl import TableInfo
+from .str_cleaning.clean_all_strcol import clean_all_strcols
+from .str_cleaning.clean_strcol import clean_strcol
