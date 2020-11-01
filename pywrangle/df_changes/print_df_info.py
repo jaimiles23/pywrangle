bc47d7626fb42757066df6deb1efc1fc2bb61989
Jai <jaimiles23@gmail.com>
2020-11-01 11:05:51 -0800

    added optional arguments for comparison & print statements on indices compared

--- pywrangle/df_changes/print_df_info.py
+++ pywrangle/df_changes/print_df_info.py
@@ -29,15 +29,24 @@ from ..print_tbl.table_class import TableInfo
 
 def print_df_info(
     *args: List[ Union['df', dict]],
-    compare_dfs: bool = True
+    compare_dfs: bool = True,
+    compare_base_df: int = 0,
+    compare_end_df: int = -1
     ) -> None:
     """Prints df informations from dfs & recorded info in args
 
     Args:
         *args (List[ Union['df', dict]]): List of dfs & dicts of df info to print info
-        compare_dfs (bool, optional): Show the differnece between the 1st & last df. Defaults to True.
-    
-    Note: Dataframes are assigned a name based on the position they are passed into *args
+        compare_dfs (bool, optional): Show the difference between 2 dataframes. 
+                                    Shows both absoluate and relative differences. 
+                                    Defaults to True.
+        compare_base_df (int): Index of base df to compare. Defaults to 0 (first df info).
+        compare_end_df (int): Index of end df to compare. Defaults to -1 (last df info).
+                    
+    NOTE: 
+    - Dataframes are assigned a name based on the index that they are passed into *args
+    - Relative (%) difference is calculated as total of base df.
+
     """
     args = list(args)
 
@@ -63,19 +72,23 @@ def print_df_info(
         if args[i][DF_KEYS[0]] is None: [DF_KEYS[0]] = i  # Change name if not indicated
         tbl_df_info.add_entry(args[i])
 
-    ## Calculate difference value, if applicable
-    if compare_dfs and len(args) > 1:
+    if compare_dfs and len(args) > 1:   ## Dataframe difference
 
-        df_diff_info = dict()
+        df_abs_diff, df_rel_diff = dict(), dict()
         for key in DF_KEYS:
             if key == DF_KEYS[0]:   # name
-                value = 'Difference'
+                val_abs, val_rel = "Abs Diff", "% Diff"
             else:
-                value = args[0][key] - args[-1][key]
-            df_diff_info[key] = value
+                val_abs = args[compare_end_df][key] - args[compare_base_df][key]
+                val_rel = round(val_abs / args[compare_base_df][key], 5) * 100
 
-        tbl_df_info.add_entry(df_diff_info)
+            df_abs_diff[key] = val_abs
+            df_rel_diff[key] = val_rel
 
-    tbl_df_info.print_info()
+        tbl_df_info.add_entries(df_abs_diff, df_rel_diff)
+
+    tbl_df_info.print_info(show_records_col= False)
+    if compare_dfs:
+        print(f"Compared DataFrames at index {compare_end_df} to {compare_base_df}")
 
 
