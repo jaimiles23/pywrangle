68148350ed65540a6d88b5c7f73cd14165d8f423
Jai <jaimiles23@gmail.com>
2020-11-02 19:18:36 -0800

    refactored similiarity index function & added second table.

--- pywrangle/str_matching/constants.py
+++ pywrangle/str_matching/constants.py
@@ -9,16 +9,18 @@ Constants for str_matching information.
 METAPHONE_KEY = 'metaphone'
 SIM_INDEX = 'Similarity Index'
 
+TBL_DICT_KEYS = (
+    'string',
+    'match',
+    SIM_INDEX
+)
 
 RATIO_DICT_KEYS = (
-    'key',
-    'match',
     'exact',
     'partial', 
     'token sort',
     'token set',
     'wratio',
     METAPHONE_KEY,
-    SIM_INDEX
 )
 
--- pywrangle/str_matching/ratios.py
+++ pywrangle/str_matching/ratios.py
@@ -8,7 +8,6 @@ Fuzzywuzzy formulas use levenshtein's distance to compare strings.
 # Imports
 ##########
 
-import statistics
 from fuzzywuzzy import fuzz, process
 from metaphone import doublemetaphone
 
@@ -41,6 +40,26 @@ def check_is_metaphone(t1: str, t2: str) -> int:
             return 1
     return 0
 
+##########
+# Similary index
+##########
+
+def calculate_similarity_index(ratios_dict: dict) -> float:
+    """Returns index representing string similarity from ratios_dict.
+    Index drops the lowest 2 matching balues to place emphasis on high values.
+    NOTE: Likely one of these is metaphone.
+
+    Args:
+        ratios_dict (dict): dictionary of ratio values
+
+    Returns:
+        float: index representing similarity b/w 2 strings
+    """
+    vals = [ v for v in ratios_dict.values() if isinstance(v, (int, float))]
+    for _ in range(2):
+        vals.remove(min(vals))
+    return round(sum(vals) / len(vals), 3)
+
 
 ##########
 # Ratio Dict
@@ -66,18 +85,15 @@ def get_ratio_dict(t1: str, t2: str) -> int:
         - Should refactor ratio_dict to not include t1 name & add that in outside method.
     """
     ratio_dict = {
-        constants.RATIO_DICT_KEYS[2]    :   fuzz.ratio(t1, t2),
-        constants.RATIO_DICT_KEYS[3]    :   fuzz.partial_ratio(t1, t2),
-        constants.RATIO_DICT_KEYS[4]    :   fuzz.token_sort_ratio(t1, t2),
-        constants.RATIO_DICT_KEYS[5]    :   fuzz.token_set_ratio(t1, t2),
-        constants.RATIO_DICT_KEYS[6]    :   fuzz.WRatio(t1, t2),
-        constants.RATIO_DICT_KEYS[7]    :   check_is_metaphone(t1, t2)
+        constants.RATIO_DICT_KEYS[0]    :   fuzz.ratio(t1, t2),
+        constants.RATIO_DICT_KEYS[1]    :   fuzz.partial_ratio(t1, t2),
+        constants.RATIO_DICT_KEYS[2]    :   fuzz.token_sort_ratio(t1, t2),
+        constants.RATIO_DICT_KEYS[3]    :   fuzz.token_set_ratio(t1, t2),
+        constants.RATIO_DICT_KEYS[4]    :   fuzz.WRatio(t1, t2),
+        constants.RATIO_DICT_KEYS[5]    :   check_is_metaphone(t1, t2)
     }
-    
-    ratio_dict[constants.SIM_INDEX] = statistics.mean([v for v in ratio_dict.values() if v != 0])     # Don't include 0 metaphone.
-    
-    return_dict = {}
-    return_dict['key'] = t1
-    return_dict['match'] = t2
-    return_dict[constants.SIM_INDEX] = round(statistics.mean([v for v in ratio_dict.values() if v != 0]), 5)
-    return return_dict
+    return {
+        TBL_DICT_KEYS[0]    :   t1,
+        TBL_DICT_KEYS[1]    :   t2,
+        TBL_DICT_KEYS[2]    :   calculate_similarity_index(ratio_dict)
+    }
